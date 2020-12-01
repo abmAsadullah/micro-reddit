77f9533eee8e0e0b3e3a3135be7a0da15ce78a0f
A B M Asadullah <abmasadullah@gmail.com>
2020-12-01 22:23:21 +0600

    Add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_153410) do
+ActiveRecord::Schema.define(version: 2020_12_01_155726) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
